f97a37a418fc40908b076f0cb3355c658dd45cb1
tonantr <tonantruong@hotmail.com>
2023-06-18 06:13:46 -0400

    complete React Information Flow Lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Header from "./Header";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
@@ -11,12 +12,13 @@ function App() {
 
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
-      <header>
+      {/* <header>
         <h2>Shopster</h2>
         <button onClick={onDarkModeClick}>
           {isDarkMode ? "Dark" : "Light"} Mode
         </button>
-      </header>
+      </header> */}
+      <Header isDarkMode={isDarkMode} onDarkModeClick={onDarkModeClick}/>
       <ShoppingList items={itemData} />
     </div>
   );
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,10 +1,15 @@
 import React, { useState } from "react";
 import Item from "./Item";
+import Filter from "./Filter";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
 
-  function handleCategoryChange(event) {
+  // function handleCategoryChange(event) {
+  //   setSelectedCategory(event.target.value);
+  // }
+
+  function onCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
@@ -16,14 +21,15 @@ function ShoppingList({ items }) {
 
   return (
     <div className="ShoppingList">
-      <div className="Filter">
+      {/* <div className="Filter">
         <select name="filter" onChange={handleCategoryChange}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
         </select>
-      </div>
+      </div> */}
+      <Filter onCategoryChange={onCategoryChange} />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
